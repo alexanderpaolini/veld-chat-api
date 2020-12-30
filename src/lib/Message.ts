813b5b4b9335b7552ca8208310504d7bbfe11236
Million900o <apaolini900o@gmail.com>
2020-12-29 23:33:59 -0500

    get user from cache

--- src/lib/Message.ts
+++ src/lib/Message.ts
@@ -1,18 +1,19 @@
 import RawMessage from '../types/RawMessage';
+import RawUser from '../types/RawUser';
 import { Channel } from './Channel';
 import { Client } from './Client';
 
 export class Message {
   id: string;
   channel: Channel;
-  user: string;
+  user: RawUser;
   content: string;
   client: Client;
   mentions: string[];
   constructor(data: RawMessage, client: Client) {
     this.id = data.id;
     this.channel = new Channel(data.channelId, client);
-    this.user = data.user;
+    this.user = client.users.get(data.user);
     this.content = data.content;
     this.client = client;
     this.mentions = data.mentions;
